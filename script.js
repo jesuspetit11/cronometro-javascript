4f0a67025b28e9ac22a666a4de4e8e21afb2729e
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-08-22 14:10:06 -0400

    inicio de proyecto

---
+++ script.js
@@ -0,0 +1,65 @@
+const cronometro = document.getElementById("cronometro");
+const botonInicioPausa = document.getElementById("boton-inicio-pausa");
+const botonReiniciar = document.getElementById("boton-reiniciar");
+
+let [horas, minutos, segundos] = [0, 0, 0];
+
+let intervaloDeTiempo;
+let estadoCronometro = "pausado";
+
+function actualizarConometro() {
+    segundos++;
+    if(segundos / 60 === 1){
+        segundos = 0;
+        minutos++;
+
+        if(minutos / 60 === 1){
+            minutos = 0;
+            horas++;
+        }
+    }
+    const segundosConFormato = asignarFormato(segundos);
+    const minutosConFormato = asignarFormato(minutos);
+    const horasConFormato = asignarFormato(horas);
+
+    cronometro.innerText = `${horasConFormato}:${minutosConFormato}:${segundosConFormato}`;
+
+}
+
+function asignarFormato(unidadDeTiempo) {
+    return unidadDeTiempo < 10 ? "0" + unidadDeTiempo : unidadDeTiempo;
+} 
+
+botonInicioPausa.addEventListener("click", function() {
+    if(estadoCronometro === "pausado"){
+        intervaloDeTiempo = window.setInterval(actualizarConometro,1000);
+        botonInicioPausa.innerHTML = '<i class="bi bi-pause-fill"></i>';
+        botonInicioPausa.classList.remove("iniciar");
+        botonInicioPausa.classList.add("pausar");
+        estadoCronometro = "andando";
+    } else {
+        window.clearInterval(intervaloDeTiempo);
+        botonInicioPausa.innerHTML = '<i class="bi bi-play-fill"></i>';
+        botonInicioPausa.classList.remove("pausar");
+        botonInicioPausa.classList.add("iniciar");
+        estadoCronometro = "pausado";
+    }
+});
+
+
+botonReiniciar.addEventListener("click", function() {
+    //Reiniciar
+    window.clearInterval(intervaloDeTiempo);
+    segundos = 0;
+    minutos = 0;
+    horas = 0;
+
+    //Actualizar botones
+    cronometro.innerText = "00:00:00";
+    botonInicioPausa.classList.remove("pausar");
+    botonInicioPausa.innerHTML = '<i class="bi bi-play-fill"></i>';
+    botonInicioPausa.classList.add("iniciar");
+
+    //Estado
+    estadoCronometro = "pausado";
+});
